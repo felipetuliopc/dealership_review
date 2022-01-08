d49d11ae3f17e66663e3f71bd6b78b80b8cbe9bf
Felipe Túlio <felipe.tulio.pc@gmail.com>
2022-01-08 16:34:33 -0300

    :recycle: Initializing logger on constructor

--- dealership_review/utils/http_client.py
+++ dealership_review/utils/http_client.py
@@ -12,6 +12,9 @@ class HttpClient:
     Wrapper for an HTTP client package
     """
 
+    def __init__(self):
+        self.logger = Logger()
+
     def get_json(self, url: str) -> dict:
         """
         Makes an HTTP GET request and return a dict of the json payload
@@ -23,7 +26,7 @@ class HttpClient:
         try:
             return response.json()
         except requests.exceptions.JSONDecodeError as exception:
-            Logger().error(exception)
+            self.logger.error(exception)
             raise FailedToDecodeFromJson() from exception
 
     def get_html(self, url: str) -> str:
@@ -42,7 +45,7 @@ class HttpClient:
         Runs a validation on the response status code and raise exceptions if needed
         """
         if response.status_code != 200:
-            Logger().error(
+            self.logger.error(
                 f'Request made to {response.request.url} returned {response.status_code}'
             )
             raise HttpRequestDidNotReturnOk
